1165e78bf99c111f520a96a8e125ceaefcf5d00a
Steve Tymoshenko <58794137+stackwebca@users.noreply.github.com>
2023-01-20 13:17:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php echo '<a href="https://www.projectsbysteve.com" target="_blank">www.projectsbysteve.com</a>' ?>
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
